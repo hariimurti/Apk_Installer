6eb09f819c5639b17f67656d083af6a4c4bea4ba
Harimurti Wibowo <hariimurti@gmail.com>
2017-06-10 13:40:24 +0700

    picturebox menggunakan zoom

--- Apk_Installer/MainForm.Designer.cs
+++ Apk_Installer/MainForm.Designer.cs
@@ -113,7 +113,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(254, 20);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(60, 60);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
